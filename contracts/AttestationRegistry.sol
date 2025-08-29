e4b70b9a8d884fcc40fbfb5f025ff6566227097f
testuem-art <testuem@gmail.com>
2025-08-29 15:22:46 +0300

    Update AttestationRegistry.sol

--- contracts/AttestationRegistry.sol
+++ contracts/AttestationRegistry.sol
@@ -10,15 +10,49 @@ contract AttestationRegistry {
     }
 
     mapping(bytes32 => Attestation[]) public assetAttestations;
+    mapping(address => bool) public approvedVerifiers;
+    address public owner;
 
     event AttestationAdded(bytes32 indexed assetHash, address indexed verifier, bytes32 dataHash);
+    event VerifierAdded(address indexed verifier);
+    event VerifierRemoved(address indexed verifier);
 
-    function addAttestation(bytes32 assetHash, bytes32 dataHash) external {
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    modifier onlyApprovedVerifier() {
+        require(approvedVerifiers[msg.sender], "Not an approved verifier");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+        approvedVerifiers[msg.sender] = true;
+    }
+
+    function addVerifier(address _verifier) external onlyOwner {
+        approvedVerifiers[_verifier] = true;
+        emit VerifierAdded(_verifier);
+    }
+
+    function removeVerifier(address _verifier) external onlyOwner {
+        approvedVerifiers[_verifier] = false;
+        emit VerifierRemoved(_verifier);
+    }
+
+    function addAttestation(bytes32 assetHash, bytes32 dataHash) external onlyApprovedVerifier {
         assetAttestations[assetHash].push(Attestation(msg.sender, dataHash, block.timestamp));
         emit AttestationAdded(assetHash, msg.sender, dataHash);
     }
 
     function getAttestationCount(bytes32 assetHash) external view returns (uint) {
         return assetAttestations[assetHash].length;
+    }
+
+    function getAttestation(bytes32 assetHash, uint index) external view returns (address verifier, bytes32 dataHash, uint256 timestamp) {
+        Attestation memory a = assetAttestations[assetHash][index];
+        return (a.verifier, a.dataHash, a.timestamp);
     }
 }
